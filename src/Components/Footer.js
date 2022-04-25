251baefe3755d4a83ce4ab6032879d2c3080f2c3
Orlayhemmy <daminomics@gmail.com>
2022-04-25 16:52:36 +0100

    remove footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,12 +24,6 @@ class Footer extends Component {
 
               <ul className="copyright">
                 <li>&copy; Copyright 2022 zSolutions Ltd</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
               </ul>
             </div>
           </Fade>
