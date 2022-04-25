636da0441bf579f1468797d14e6b4b13795ca6a2
Orlayhemmy <daminomics@gmail.com>
2022-04-25 16:51:30 +0100

    change title

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="zSolutions Limited Pic"
               />
             </div>
             <div className="nine columns main-col">
